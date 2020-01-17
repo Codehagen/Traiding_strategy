afda36d66bf21b560dd151ef0cfa101779dd4e1b
assroll <Christer.Hagen@gmail.com>
2020-01-17 19:20:32 +0100

    live autofeed fikset

--- livedata.py
+++ livedata.py
@@ -6,25 +6,29 @@ from alpha_vantage.techindicators import TechIndicators
 import matplotlib.pyplot as plt
 
 print("Updating stocks...")
-#Din key fra alpha
+
+#Your Key
 key = 'LHFZM9FAGF3J9RV2'
 
 def AlphaVantage(symbol):
-    ts = TimeSeries(key)
-    data = ts.get_intraday(symbol, interval='1min')
+    ts = TimeSeries(key, output_format='pandas')
+    data = ts.get_intraday(symbol, interval='5min', outputsize='full')
+    #Chart
+    
 
     print(str(data))
+    print("Updating stocks...10 seconds to next update")
 
- #Skriv inn hvordan aksje du vil ha
+#What Stock
 AlphaVantage('AMD')
 
-def job():
-   print("I'm working...")
 
 
 
-#Timer på updates
-schedule.every(1).seconds.do(job)
+
+
+#Timer on updates
+schedule.every(10).seconds.do(AlphaVantage, symbol='AMD')
 
 while 1:
    schedule.run_pending()
